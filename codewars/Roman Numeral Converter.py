5eb12dd5f60168d195b1600a17f9f686f600d8c3
devmgardner <devm.gardner@gmail.com>
2021-06-16 20:03:24 +0000

    more tests needed

--- codewars/Roman Numeral Converter.py
+++ codewars/Roman Numeral Converter.py
@@ -11,13 +11,31 @@ def solution(roman) :
     }
     result = 0
     newl = list(roman)
-    count = 0
-    while count < len(newl)-1 :
-        if nums.get(newl[count]) >= nums.get(newl[count+1]) :
-            result += nums.get(newl[count])
-            count += 1
-        elif nums.get(newl[count]) < nums.get(newl[count+1]) :
-            result += (nums.get(newl[count+1]) - nums.get(newl[count]))
-            count += 1
+    for item in newl :
+        valueofchar = nums.get(item)
+        lastindex = len(newl) - 1
+        posofchar = newl.index(item)
+        prevchar = posofchar-1
+        nextchar = posofchar+1
+        prevval = nums.get(newl[prevchar])
+        if not posofchar == lastindex :
+            nextval = nums.get(newl[nextchar])
+        if len(newl) == 1 : result = valueofchar
+        elif posofchar == lastindex and not len(newl) == 1 and not valueofchar > prevval : 
+            result += valueofchar
+        elif not posofchar == 0 and valueofchar > prevval : continue
+        elif posofchar == lastindex and valueofchar > prevval : continue
+        elif valueofchar >= nextval :
+            result += valueofchar
+        elif valueofchar < nextval :
+            result += (nextval - valueofchar)
     return result
+#    while count < len(newl) :
+#        if nums.get(newl[count]) >= nums.get(newl[count+1]) :
+#            result += nums.get(newl[count])
+#            count += 1
+#        elif nums.get(newl[count]) < nums.get(newl[count+1]) :
+#            result += (nums.get(newl[count+1]) - nums.get(newl[count]))
+#            count += 1
+#    return result
 print(solution(inp))
