fa63bfaf78d0f80b7f2abfe4684d158e78b3858c
devmgardner <devm.gardner@gmail.com>
2021-06-17 18:41:34 +0000

    it's even more complicated now

--- codewars/Roman Numeral Converter.py
+++ codewars/Roman Numeral Converter.py
@@ -1,14 +1,14 @@
 inp = input('')
 def solution(roman) :
-    nums = [
-        ('I', 1, 1),
-        ('V', 5, 2),
-        ('X', 10, 3),
-        ('L', 50, 4),
-        ('C', 100, 5),
-        ('D', 500, 6),
-        ('M', 1000, 7),
-    ]
+    nums = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    }
     valid = {
         'IV' : 4,
         'IX' : 9,
@@ -21,22 +21,48 @@ def solution(roman) :
     newl = list(roman)
     for item in newl :
         valueofchar = nums.get(item)
+        print(f'char is {item}')
+        print(f'valueofchar is {valueofchar}')
         lastindex = len(newl) - 1
         posofchar = newl.index(item)
-        prevchar = posofchar-1
-        nextchar = posofchar+1
-        prevval = nums.get(newl[prevchar])
+        print(f'posofchar is {posofchar}')
+        if not posofchar == 0 :
+            prevchar = posofchar-1
+            prevval = nums.get(newl[prevchar])
+            print(f'prevchar is {newl[prevchar]}')
+            print(f'prevval is {prevval}')
+        else :
+            prevchar = None
+            prevval = None
         if not posofchar == lastindex :
+            nextchar = posofchar+1
             nextval = nums.get(newl[nextchar])
+            print(f'nextchar is {newl[nextchar]}')
+            print(f'nextval is {nextval}')
+        else :
+            nextchar = None
+            nextval = None
         if len(newl) == 1 : result = valueofchar
-        elif posofchar == lastindex and not len(newl) == 1 and not valueofchar > prevval : 
+        elif item + newl[nextchar] in valid :
+            print('char and next are combo, adding')
+            result += valid.get(item + newl[nextchar])
+            print(f'current total is {result}')
+        elif not prevchar is None and newl[prevchar] + item in valid : continue
+        elif posofchar == lastindex and not len(newl) == 1 and not newl[prevchar] + item in valid :
+            print('char is final character and is not combo with prevchar, adding')
             result += valueofchar
-        elif not posofchar == 0 and valueofchar > prevval : continue
-        elif posofchar == lastindex and valueofchar > prevval : continue
-        elif valueofchar >= nextval :
+            print(f'current total is {result}')
+            continue
+        #elif posofchar == lastindex and not len(newl) == 1 and not valueofchar > prevval : 
+            #result += valueofchar
+        #elif not posofchar == 0 and valueofchar > prevval : continue
+        #elif posofchar == lastindex and valueofchar > prevval : continue
+        elif not nextval is None and valueofchar >= nextval :
+            print('char is higher than nextchar, adding')
             result += valueofchar
-        elif valueofchar < nextval :
-            result += (nextval - valueofchar)
+            print(f'current total is {result}')
+        #elif valueofchar < nextval :
+            #result += (nextval - valueofchar)
     return result
 #    while count < len(newl) :
 #        if nums.get(newl[count]) >= nums.get(newl[count+1]) :
