ab085d31e7331b94bc5a97cfcacae50694de2680
devmgardner <devm.gardner@gmail.com>
2021-06-21 18:23:43 +0000

    asd

--- codewars/Persistent Bugger/Persistent Bugger.py
+++ codewars/Persistent Bugger/Persistent Bugger.py
@@ -4,15 +4,19 @@ def persistence(n):
     numl = list(str(n))
     if len(numl) == 1 : return 0
     total = 1
+    print(f'n is {n}')
     for item in numl :
+        print(f'---\t{item} is current digit')
         total *= int(item)
-        print(f'current total is {total}')
-        print(f'length of current total is {len(str(total))}')
-    print(f'final total is {total}')
-    print(f'length of final total is {len(str(total))}')
+        print(f'-----\t{total} is current total after multiplying by {item}')
+        print(f'-----\t{len(str(total))} is length of current total')
+    print(f'--\tfinal total is {total} with length of {len(str(total))}')
+    print(f'--\tlength of final total is {len(str(total))}')
     if len(str(total)) > 1 :
+        print(f'{count} is count after checking that length of current total is above 1')
         global count; count += 1
-        print(f'length of total({total}) is > 1, running again. current count is {count}')
+        print(f'{count} is count after running global count; count += 1')
+        print(f'-------\tlength of total({total}) is > 1, running again. current count is {count}')
         persistence(total)
     return count
     #while len(str(total)) > 1:
