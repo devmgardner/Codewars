1583e0b10363a80b85e4a8e93d03b8e84cbaf423
devmgardner <devm.gardner@gmail.com>
2021-06-22 16:04:37 -0400

    starting from scratch

--- codewars/Hex to Base64 Converter/newtest.py
+++ codewars/Hex to Base64 Converter/newtest.py
@@ -1,6 +1,18 @@
+#for now, print a line to a file at every single opportunity to debug the whole program step by step
+#number each function/thing that happens so debugging is easier
+#all variable names should be long and descriptive until program works, then variable names can be shortened
+#keep everything as spaced out as possible to be easier to read
 hexalph = """!"#$%&'()*+,-./0123456789:'<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"""
+fname = 'testoutput.txt'
+fhand = open(fname, 'w')
+fhand.close()
+fhand = open(fname, 'r+')
 def hexto64(hex_string) :
-    n = list(hex_string.lower())
-    n.reverse()
+    fhand.write(f'{hex_string} is input at beginning of function\n')
+    hexlist = list(hex_string.lower())
+    fhand.write(f'{hexlist} is lowercase list\n')
+    hexlist.reverse()
+    fhand.write(f'{hexlist} is reversed list\n')
     nlist = []
-    
+    for char in n :
+        fhand.write(f'{char} is current char in n')
