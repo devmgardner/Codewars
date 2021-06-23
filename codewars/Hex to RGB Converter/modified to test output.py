687edc776c3258c9f9a457348d4e9058f3c4e95a
devmgardner <devm.gardner@gmail.com>
2021-06-23 13:06:46 -0400

    testing functions

--- codewars/Hex to RGB Converter/modified to test output.py
+++ codewars/Hex to RGB Converter/modified to test output.py
@@ -6,16 +6,15 @@ fhand = open(fname, 'r+')
 def hex_string_to_RGB(hex_string): 
     def hextodec(n) :
         total = 0
-        fhand.write(f'{n} IS CURRENT PAIR OF CHARACTERS\n')
+        fhand.write(f'\n\n{n} IS CURRENT PAIR OF CHARACTERS\n')
         n = list(n.lower())
         n.reverse()
         print(f'after split and reverse, n is {n}\n')
-        fhand.write(f'{n} is pair of characters after split and reverse\n')
         for ind, char in enumerate(n) :
-            fhand.write(f'{ind},{char} is current index and character\n')
-            fhand.write(f'{hexalph.index(char)} is index of character\n')
-            fhand.write(f'{16 ** ind} is 16^ind\n')
-            fhand.write(f'{(hexalph.index(char) * (16 ** ind))} is decimal form of hexadecimal input\n')
+            fhand.write(f'{ind},{char} is current index and character in input\n')
+            fhand.write(f'{hexalph.index(char)} is index of character in hexalph list\n')
+            fhand.write(f'{16 ** ind} is 16 ^ index\n')
+            fhand.write(f'{(hexalph.index(char) * (16 ** ind))} is decimal form of hexadecimal input\n\n')
             total += (hexalph.index(char) * (16 ** ind))
         return total
     return {
