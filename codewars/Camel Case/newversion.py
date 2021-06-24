35cf6725fb3f5d481a5f7fede6ded041ad4153b7
devmgardner <devm.gardner@gmail.com>
2021-06-24 12:32:46 +0000

    Camel Case completed

--- codewars/Camel Case/newversion.py
+++ codewars/Camel Case/newversion.py
@@ -1,4 +1,3 @@
-import re
 fname = 'camelcaselog.txt'
 fhand = open(fname, 'w')
 fhand.close()
@@ -7,10 +6,16 @@ def to_camel_case(text):
     #your code here
     if len(text) == 0 : return ''
     sent = []
-    words = re.split('[-_]', text)
-    for word in words :
-        fhand.write(f'{word}, ')
-    fhand.write(f'\n')
+    if '-' in text :
+        words = text.split('-')
+        for word in words :
+            fhand.write(f'{word}, ')
+        fhand.write(f'\n')
+    elif '_' in text :
+        words = text.split('_')
+        for word in words :
+            fhand.write(f'{word}, ')
+        fhand.write(f'\n')
     if words[0][0].isupper() == True :
         for word in words :
             word = word[:1].upper() + word[1:]
--- codewars/Camel Case/submitted.py
+++ codewars/Camel Case/submitted.py
@@ -1,30 +1,17 @@
 import re
-fname = 'camelcaselog.txt'
-fhand = open(fname, 'w')
-fhand.close()
-fhand = open(fname, 'r+')
 def to_camel_case(text):
     #your code here
     if len(text) == 0 : return ''
     sent = []
     words = re.split('[-_]', text)
-    for word in words :
-        fhand.write(f'{word}, ')
-    fhand.write(f'\n')
     if words[0][0].isupper() == True :
         for word in words :
             word = word[:1].upper() + word[1:]
-            fhand.write(f'{word}, ')
             sent.append(word)
-        fhand.write(f'\n')
     else : 
         sent.append(words[0])
         for word in words[1:] :
             word = word[:1].upper() + word[1:]
-            fhand.write(f'{word}, ')
             sent.append(word)
-        fhand.write(f'\n')
-    delimiter = ''
-    sent = delimiter.join(sent)
+    sent = ''.join(sent)
     return sent
-print(to_camel_case(input('')))
