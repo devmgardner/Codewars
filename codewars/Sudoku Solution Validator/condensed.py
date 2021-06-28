d85de71ab031786f36a1cee1ddb7377aebbfebf3
devmgardner <devm.gardner@gmail.com>
2021-06-28 10:17:07 -0400

    further compression

--- codewars/Sudoku Solution Validator/condensed.py
+++ codewars/Sudoku Solution Validator/condensed.py
@@ -1,6 +1,6 @@
 def valid_solution(board):
-    grids=[[item for row in board[:3] for item in row[:3]], [item for row in board[3:6] for item in row[:3]], [item for row in board[6:9] for item in row[:3]], [item for row in board[:3] for item in row[3:6]], [item for row in board[3:6] for item in row[3:6]], [item for row in board[6:9] for item in row[3:6]], [item for row in board[:3] for item in row[6:9]], [item for row in board[3:6] for item in row[6:9]], [item for row in board[6:9] for item in row[6:9]]]
-    def checkrow(n):return all(i for i in [True if row.count(item)==1 and str(item) in ['1','2','3','4','5','6','7','8','9'] else False for row in n for item in row])
-    def checkcol(n):return all(i for i in [True if column.count(item)==1 and str(item) in ['1','2','3','4','5','6','7','8','9'] else False for column in [[row[i] for row in n] for i in range(9)] for item in column])
-    def checkgrid(n):return all(i for i in [True if grid.count(item)==1 and str(item) in ['1','2','3','4','5','6','7','8','9'] else False for grid in grids for item in grid])
-    return all(i for i in [checkrow(board), checkcol(board), checkgrid(board)])
+    gs=[[x for r in board[:3] for x in r[:3]], [x for r in board[3:6] for x in r[:3]], [x for r in board[6:9] for x in r[:3]], [x for r in board[:3] for x in r[3:6]], [x for r in board[3:6] for x in r[3:6]], [x for r in board[6:9] for x in r[3:6]], [x for r in board[:3] for x in r[6:9]], [x for r in board[3:6] for x in r[6:9]], [x for r in board[6:9] for x in r[6:9]]]
+    def cr(n):return all(i for i in [True if r.count(x)==1 and str(x) in ['1','2','3','4','5','6','7','8','9'] else False for r in n for x in r])
+    def cc(n):return all(i for i in [True if c.count(x)==1 and str(x) in ['1','2','3','4','5','6','7','8','9'] else False for c in [[r[i] for r in n] for i in range(9)] for x in c])
+    def cg(n):return all(i for i in [True if g.count(x)==1 and str(x) in ['1','2','3','4','5','6','7','8','9'] else False for g in gs for x in g])
+    return all(i for i in [cr(board), cc(board), cg(board)])
