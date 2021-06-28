3cb7b200201442968039a8bc5235cbeaa2dff663
devmgardner <devm.gardner@gmail.com>
2021-06-28 02:28:52 +0000

    Roman Numerals Helper started

---
+++ codewars/Roman Numerals Helper/Roman Numerals Helper.py
@@ -0,0 +1,27 @@
+fname = 'Roman Numerals Helper-log.txt'
+fhand = open(fname, 'w')
+fhand.close()
+fhand = open(fname, 'r+')
+
+class RomanNumerals():
+    def to_roman(self):
+        try:
+            self = int(self)
+        except:
+            print('Invalid, quitting.')
+            quit()
+    
+
+    def from_roman(self):
+
+
+
+
+
+
+
+
+
+
+
+print((input('')))
