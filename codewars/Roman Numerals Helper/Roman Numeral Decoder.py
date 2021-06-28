df5de644e215617027796b839934f539da0b6bc0
devmgardner <devm.gardner@gmail.com>
2021-06-28 14:23:11 -0400

    condensed more

--- codewars/Roman Numerals Helper/Roman Numeral Decoder.py
+++ codewars/Roman Numerals Helper/Roman Numeral Decoder.py
@@ -5,61 +5,19 @@ fhand = open(fname, 'r+')
 
 inp = input('')
 def solution(roman) :
-    nums = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-    valid = {'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900}
-    
     result = 0
     newl = list(roman)
     for (ind, item) in enumerate(newl) :
-        fhand.write(f'char is {item}')
-        fhand.write(f'nums.get(item) is {nums.get(item)}')
-        if ind > 0 :
-            prev = newl[ind - 1]
-        if ind < (len(newl) - 1) :
-            nex = newl[ind + 1]
-        fhand.write(f'ind is {ind}')
-        if not ind == 0 :
-            prevchar = ind-1
-            prevval = nums.get(newl[prevchar])
-            fhand.write(f'prevchar is {newl[prevchar]}')
-            fhand.write(f'prevval is {prevval}')
-        else :
-            prevchar = None
-            prevval = None
-        if not ind == (len(newl) - 1) :
-            nextchar = ind+1
-            nextval = nums.get(newl[nextchar])
-            fhand.write(f'nextchar is {newl[nextchar]}')
-            fhand.write(f'nextval is {nextval}')
-        else :
-            nextchar = None
-            nextval = None
-        if len(newl) == 1 : result = nums.get(item)
-        elif not ind == (len(newl) - 1) and item + newl[nextchar] in valid :
-            fhand.write('char and next are combo, adding')
-            result += valid.get(item + newl[nextchar])
-            fhand.write(f'current total is {result}')
-        elif not prevchar is None and newl[prevchar] + item in valid : continue
-        elif ind == (len(newl) - 1) and not len(newl) == 1 and not newl[prevchar] + item in valid :
-            fhand.write('char is final character and is not combo with prevchar, adding')
-            result += nums.get(item)
-            fhand.write(f'current total is {result}')
+        if len(newl) == 1 : result = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}.get(item)
+        #combo character
+        elif not ind == (len(list(roman)) - 1) and item + list(roman)[ind+1] in {'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900} :
+            result += {'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900}.get(item + list(roman)[ind+1])
+        elif not list(roman)[ind-1] is None and list(roman)[ind-1] + item in {'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900} : continue
+        #last character, not combo
+        elif ind == (len(list(roman)) - 1) and not len(list(roman)) == 1 and not list(roman)[ind-1] + item in {'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900} :
+            result += {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}.get(item)
             continue
-        elif ind != (len(newl) - 1) and nums.get(item) >= nextval :
-            fhand.write(f'ind is {ind}')
-            fhand.write(f'(len(newl) - 1) is {(len(newl) - 1)}')
-            fhand.write(f'nums.get(item) is {nums.get(item)}')
-            fhand.write(f'nextval is {nextval}')
-            fhand.write('char is higher than nextchar, adding')
-            result += nums.get(item)
-            fhand.write(f'current total is {result}')
+        elif ind != (len(list(roman)) - 1) and {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}.get(item) >= {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}.get(list(roman)[ind+1]) :
+            result += {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}.get(item)
     return result
-#    while count < len(newl) :
-#        if nums.get(newl[count]) >= nums.get(newl[count+1]) :
-#            result += nums.get(newl[count])
-#            count += 1
-#        elif nums.get(newl[count]) < nums.get(newl[count+1]) :
-#            result += (nums.get(newl[count+1]) - nums.get(newl[count]))
-#            count += 1
-#    return result
 fhand.write(solution(inp))
