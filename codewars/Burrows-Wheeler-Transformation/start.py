5ada811b22967b341fb46ef72cb9645c629e6983
devm.gardner <devm.gardner@gmail.com>
2022-07-01 17:22:22 +0000

    started on Burrows-Wheeler-Transformation

---
+++ codewars/Burrows-Wheeler-Transformation/start.py
@@ -0,0 +1,6 @@
+#https://www.codewars.com/kata/54ce4c6804fcc440a1000ecb/train/python
+def encode(s):
+    return
+
+def decode(s, n):
+    return
---
+++ codewars/Burrows-Wheeler-Transformation/working.py
@@ -0,0 +1,48 @@
+def encode(s):
+    def create_matrix(string):
+        slist,length = list(string), len(string)
+        matrix = []
+        matrix.append(slist)
+        #print(f'starting matrix is:')
+        #print(matrix)
+        def caesar_shift(inp):
+            return inp[-1:] + inp[:-1]
+        for i in range(length-1):
+            slist = caesar_shift(slist)
+            matrix.append(slist)
+            #print(f'after iteration {i}, matrix is:')
+            #for t in matrix:
+            #    print(t)
+        return matrix
+    matrix = create_matrix(s)
+    print(f'initial matrix is:')
+    for i in matrix:
+        print(i)
+    matrix = sorted(matrix)
+    print(f'sorted matrix is:')
+    for i in matrix:
+        print(i)
+    final_column = ''.join([i[-1] for i in matrix])
+    #print(f'final column is {final_column}')
+    final_index = matrix.index(list(s))
+    #print(f'final index is {final_index}')
+    return (final_column,final_index)
+print(encode('bananabar'))
+
+
+def decode(s, n):
+    matrix = []
+    for i in range(len(s)):
+        row = []
+        for i in range(len(s)):
+            row.append('0')
+        matrix.append(row)
+    for ind,char in enumerate(list(s)):
+        matrix[ind][len(s)-1] = char
+    for ind,char in enumerate(sorted(list(s))):
+        matrix[ind][0] = char
+    matrix[4] = list('bananabar')
+    for i in matrix:
+        print(i)
+
+decode('nnbbraaaa',4)
