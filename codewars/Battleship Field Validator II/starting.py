714be1ab9905b2bd043891bcc90756f367ba8fd5
devm.gardner <devm.gardner@gmail.com>
2022-07-03 15:08:33 +0000

    considering Battleship Field Validator II

---
+++ codewars/Battleship Field Validator II/starting.py
@@ -0,0 +1,4 @@
+#https://www.codewars.com/kata/571ec81d7e8954ce1400014f/train/python
+def validate_battlefield(battlefield):
+    # write your code
+    return True
---
+++ codewars/Battleship Field Validator II/working.py
@@ -0,0 +1,8 @@
+#https://www.codewars.com/kata/571ec81d7e8954ce1400014f/train/python
+def validate_battlefield(battlefield):
+    for row_num,row in enumerate(battlefield):
+        for col_num,cell in enumerate(row):
+            if cell == 1:
+                
+
+    return False
